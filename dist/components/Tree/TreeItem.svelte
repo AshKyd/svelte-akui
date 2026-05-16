688f5bf500a2afa22262275fbac24e75af806340
Ash Kyd <ash@kyd.com.au>
2026-05-16 15:03:00 +1000

    feat: unread item support

--- dist/components/Tree/TreeItem.svelte
+++ dist/components/Tree/TreeItem.svelte
@@ -94,6 +94,9 @@
 			{:else if iconSnippet}
 				{@render iconSnippet(item)}
 			{:else if item.icon}
+				{#if item.icon.startsWith('http')}
+					{console.warn('TreeItem - URL icon passed to Icon fallback:', item.icon, 'Snippet present:', !!iconSnippet)}
+				{/if}
 				<Icon name={item.icon} size={14} />
 			{/if}
 		</div>
